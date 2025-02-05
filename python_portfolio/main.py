b1b75ece8f545307be2c083aeacf86e3a11d426d
David Faidley da026799 <da026799@mxrgcc.mail.onmicrosoft.us>
2025-02-05 07:37:56 -0700

    started building portfolio

---
+++ python_portfolio/main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("David Faidley")
+    content = """I am David"""
+    st.info(content)
