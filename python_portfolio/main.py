e2170337a15eb17a1bbf551775b4bf89034525a7
David Faidley da026799 <da026799@mxrgcc.mail.onmicrosoft.us>
2025-02-06 08:55:37 -0700

    added pandas to code

--- python_portfolio/main.py
+++ python_portfolio/main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -12,3 +12,18 @@ with col2:
     st.title("David Faidley")
     content = """I am David"""
     st.info(content)
+
+content2 = """Bewlow you will see a list of my apps built in Python. Feel free to contact me."""
+st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+   for index, row in df[:10].interrows():
+       st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].interrow():
+        st.header(row["title"])
